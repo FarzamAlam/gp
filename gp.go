2362be96ad3ccc51892d36b022b206a6bdf17618
FarzamAlam <farzamcse@gmail.com>
2020-08-30 12:57:47 +0530

    Added method to generate stats when pinger finshes

--- gp.go
+++ gp.go
@@ -1,10 +1,15 @@
 package pg
 
 import (
+	"log"
 	"math"
 	"math/rand"
 	"net"
 	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 )
 
 const (
@@ -12,6 +17,11 @@ const (
 	trackerLen       = 8
 	protocolICMP     = 1
 	protocolIPv6ICMP = 58
+)
+
+var (
+	ipv4Proto = map[string]string{"ip": "ip4:icmp", "udp": "udp4"}
+	ipv6Proto = map[string]string{"ip": "ip6:ipv6-icmp", "udp": "udp6"}
 )
 
 // Pinger represents ICMP packet sender/receiver.
@@ -29,6 +39,7 @@ type Pinger struct {
 	Source         string
 	done           chan bool
 
+	rtts     []time.Duration
 	ipaddr   *net.IPAddr
 	addr     string
 	ipv4     bool
@@ -90,4 +101,83 @@ func NewPinger(addr string) (*Pinger, error) {
 
 func isIPv4(ip net.IP) bool {
 	return net.IPv4len == len(ip.To4())
+}
+
+func (p *Pinger) Run() {
+	var conn *icmp.PacketConn
+	if p.ipv4 {
+		conn = p.listen(ipv4Proto[p.network])
+		if conn == nil {
+			return
+		}
+		conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true)
+	} else {
+		conn = p.listen(ipv6Proto[p.network])
+		if conn == nil {
+			return
+		}
+		conn.IPv6PacketConn().SetControlMessage(ipv6.FlagHopLimit, true)
+	}
+	defer conn.Close()
+	defer p.finish()
+}
+
+// GenerateStats returns the statistics of the pinger. This can be run while
+// Pinger is runnig or after it is finished.
+// OnFinish calls this func to get its finished stats.
+func (p *Pinger) GenerateStats() *Stats {
+	loss := float64(p.PacketsSent-p.PacketsRecieve) / float64(p.PacketsSent) * 100
+	var min, max, total time.Duration
+
+	if len(p.rtts) > 0 {
+		min = p.rtts[0]
+		max = p.rtts[0]
+	}
+	for _, rtt := range p.rtts {
+		if rtt < min {
+			min = rtt
+		}
+		if rtt > max {
+			max = rtt
+		}
+		total += rtt
+	}
+	s := Stats{
+		PacketsSent:    p.PacketsSent,
+		PacketsRecieve: p.PacketsRecieve,
+		PacketsLoss:    loss,
+		Rtts:           p.rtts,
+		Addr:           p.addr,
+		IPAddr:         p.ipaddr,
+		MaxRtt:         max,
+		MinRtt:         min,
+	}
+	if len(p.rtts) > 0 {
+		s.AvgRtt = total / time.Duration(len(p.rtts))
+		var sumSquares time.Duration
+		for _, rtt := range p.rtts {
+			sumSquares += (rtt - s.AvgRtt) * (rtt - s.AvgRtt)
+		}
+		s.StdDevRtt = time.Duration(math.Sqrt(float64(sumSquares / time.Duration(len(p.rtts)))))
+	}
+	return &s
+}
+
+// finish method is called after the pinger stops.
+func (p *Pinger) finish() {
+	handler := p.OnFinish
+	if handler != nil {
+		s := p.GenerateStats()
+		handler(s)
+	}
+}
+
+func (p *Pinger) listen(netProto string) *icmp.PacketConn {
+	conn, err := icmp.ListenPacket(netProto, p.Source)
+	if err != nil {
+		log.Println("Error listening for ICMP Packets: %s\n", err.Error())
+		close(p.done)
+		return nil
+	}
+	return conn
 }
